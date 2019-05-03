d81da0876706e1bc33b4cf0a51150e727623c88f
igor.frey <igor.frey@dataprev.gov.br>
2019-05-03 15:10:45 -0300

    Creating project and repository

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import modelo.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "Books");
+		System.out.println(obj);
+	}
+
+}
