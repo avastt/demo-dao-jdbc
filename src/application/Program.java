0f8f22f56f323b92e39adf1d9d5cc99f1037704b
igor.frey <igor.frey@dataprev.gov.br>
2019-05-31 11:34:36 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,23 +17,29 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n=== TESTE 1: seller findByDepartment ===");
+		System.out.println("\n=== TESTE 2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TESTE 1: seller findAll ===");
+		System.out.println("\n=== TESTE 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TESTE 1: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! new id = " + newSeller.getId());
+//		System.out.println("\n=== TESTE 4: seller insert ===");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TESTE 5: seller insert ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Bob");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	
 	}
 }
